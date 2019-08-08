55a9e91b13bfe1243d9b116725f7987ec6b9529f
Jose Guerrero Roman <joseguerreroroman@MacBook-Pro-de-Jose.local>
2019-08-08 13:38:39 -0500

    Graph installed & router view

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,51 @@
 <template>
   <v-app>
-    <v-app-bar app>
+    <v-app-bar
+      color="#2c5583"
+      dark
+      app
+    >
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">Comparativa de costos</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-img
+        src="../src/assets/logo_global_final_cert_fondo_azul.png"
+        height="40"
+        width="40"
+        contain
+      ></v-img>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
+
+    <v-footer
+      padless
+      dark
+      color="#2c5583"
+      height="60"
+    >
+      <v-flex
+        text-center
+        xs-12
+      >
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-flex>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import MainView from './components/MainView.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    MainView
   },
   data: () => ({
     //
-  }),
-};
+  })
+}
 </script>
---
+++ src/components/MainView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container ma-1>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex xs12 sm6>
+        <apexchart width="500" type="bar" :options="options" :series="series"></apexchart>
+      </v-flex>
+
+      <v-flex mb-4>
+
+      </v-flex>
+
+ 
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script src='./MainView.js' />
